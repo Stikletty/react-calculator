ade94b6228047abf8c106b87adfb37d1f8d9e1f3
Gabor Troll <gabor.troll@gekko-marketing-com>
2021-09-29 00:14:19 +0200

    button state

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [testdata, setMessage] = useState(""); 
+  
   return (
     <section className="App h-screen w-full flex justify-center items-center bg-green-500">
       <div className="w-full max-w-md bg-gray-800">
-        <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
-          <div className="px-4 pb-4">
-            <label htmlFor="email" className="text-sm block font-bold  pb-2">
-              {" "}
-              EMAIL ADDRESS{" "}
-            </label>{" "}
-            <input
-              type="email"
-              name="email"
-              id=""
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 "
-              placeholder="Johnbull@example.com"
-            />
-          </div>{" "}
-          <div className="px-4 pb-4">
-            <label htmlFor="password" className="text-sm block font-bold pb-2">
-              {" "}
-              PASSWORD{" "}
-            </label>{" "}
-            <input
-              type="password"
-              name="email"
-              id=""
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300"
-              placeholder="Enter your password"
-            />
-          </div>{" "}
-          <div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-              {" "}
-              Sign In{" "}
-            </button>{" "}
-          </div>{" "}
-        </form>{" "}
-      </div>{" "}
+        <div className="container">
+          <div className="bg-gray-500 text-gray-900 pb-5 pt-5 font-black text-6xl text-right px-2 ">3.560</div>
+          <div className="flex flex-row ">
+            <button onClick={(e) => setMessage(e.target.innerHTML)} className="flex-col bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-full">1</button>
+            <button className="flex-col bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-full">2</button>
+            <button className="flex-col bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-full">3</button>
+            <button className="flex-col bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-full">4</button>
+          </div>
+        </div>
+      </div>      
+      <br/>
+      <div className="flex flex-row" id="testdata">{testdata}</div>
     </section>
   );
 }
